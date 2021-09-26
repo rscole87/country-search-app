75754397736eeb028979f8637132dbe36f78d86a
Rodrick Coleman <rscole87@gmail.com>
2021-09-26 10:36:05 -0400

    add countryCard component

---
+++ src/components/CountryCardComponent.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+const CountryCard = ({country, setActiveCountry}) => {
+    return (
+        <Link to={`/countries/${country.alpha3Code}`} key={country.alpha3Code}>
+        <div
+          className="country-card"
+          onClick={() => {
+            setActiveCountry(country);
+          }}
+        >
+          <div className="flag-div-sm" style={{ backgroundImage: `url(${country.flags[0]})`, backgroundPosition: "center", backgroundSize: "cover" }} />
+
+          <div className="card-content-div">
+            <h3 className="country-card-heading">{country.name}</h3>
+            <p>
+              <span className="data-label">Population:</span> {country.population}
+            </p>
+            <p>
+              <span className="data-label">Region:</span> {country.continent}
+            </p>
+            <p>
+              <span className="data-label">Capital:</span> {country.capital}
+            </p>
+          </div>
+        </div>
+      </Link>
+    )
+}
+
+export default CountryCard;
--- src/components/ResultsAreaComponent.js
+++ src/components/ResultsAreaComponent.js
@@ -1,32 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import CountryCard from "./CountryCardComponent";
 
 const ResultsArea = (props) => {
   const countryCards = props.countries.map((country) => {
     return (
-      <Link to={`/countries/${country.alpha3Code}`} key={country.alpha3Code}>
-        <div
-          className="country-card"
-          onClick={() => {
-            props.setActiveCountry(country);
-          }}
-        >
-          <div className="flag-div-sm" style={{ backgroundImage: `url(${country.flags[0]})`, backgroundPosition: "center", backgroundSize: "cover" }} />
-
-          <div className="card-content-div">
-            <h3 className="country-card-heading">{country.name}</h3>
-            <p>
-              <span className="data-label">Population:</span> {country.population}
-            </p>
-            <p>
-              <span className="data-label">Region:</span> {country.continent}
-            </p>
-            <p>
-              <span className="data-label">Capital:</span> {country.capital}
-            </p>
-          </div>
-        </div>
-      </Link>
+      <CountryCard country={country} setActiveCountry={props.setActiveCountry}/>
     );
   });
 
