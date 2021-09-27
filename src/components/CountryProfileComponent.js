c78a38532e907c104ebb9597e4c8e81fdcedd976
Rodrick Coleman <rscole87@gmail.com>
2021-09-26 21:05:13 -0400

    add styling for countryProfile component

--- src/components/CountryProfileComponent.js
+++ src/components/CountryProfileComponent.js
@@ -7,8 +7,8 @@ const CountryProfile = ({ country, setActiveCountry, resultsList }) => {
   const neighbors = resultsList.filter((result) => {
     if (country.borders) {
       return country.borders.includes(result.alpha3Code);
-    } 
-    return
+    }
+    return;
   });
   const neighborLinks = neighbors.map((neighbor) => {
     return (
@@ -17,59 +17,75 @@ const CountryProfile = ({ country, setActiveCountry, resultsList }) => {
       </Link>
     );
   });
+  
+  let currencies;
 
-  const currencies = country.currencies.map((currency) => currency.name).join(", ");
+  if (country.currencies){
+    currencies = country.currencies.map((currency) => currency.name).join(", ");
+  } else {
+    currencies = ["None"]
+  }
+
   const languages = country.languages.map((language) => language.name).join(", ");
 
   return (
     <>
       <section id="country-profile-section">
-        <div>
-          <Link to="/">
-            <button onClick={() => setActiveCountry(null)}>Back</button>
-          </Link>
-        </div>
+        <div className="container">
+          <div>
+            <Link to="/">
+              <button className="back-bttn" onClick={() => setActiveCountry(null)}>Back</button>
+            </Link>
+          </div>
 
-        <div>
-          <img src={country.flags[1]} alt={`${country.name} Flag`} />
-        </div>
-        <div>
-          <h2>{country.name}</h2>
-        </div>
+          <div className="profile-content">
+            <div className="profile-flag-div">
+              <img src={country.flags[1]} alt={`${country.name} Flag`} />
+            </div>
 
-        <div>
-          <p>
-            <span className="data-label">Native Name: </span> {country.nativeName}
-          </p>
-          <p>
-            <span className="data-label">Population: </span> {country.population}
-          </p>
-          <p>
-            <span className="data-label">Region: </span> {country.continent}
-          </p>
-          <p>
-            <span className="data-label">Sub Region: </span> {country.region}
-          </p>
-          <p>
-            <span className="data-label">Capital: </span> {country.capital}
-          </p>
-        </div>
+            <div className="profile-text-div">
+              <div className="profile-header-div">
+                <h2>{country.name}</h2>
+              </div>
 
-        <div>
-          <p>
-            <span className="data-label">Top Level Domain: </span> {country.topLevelDomain[0]}
-          </p>
-          <p>
-            <span className="data-label">Currencies: </span> {currencies}
-          </p>
-          <p>
-            <span className="data-label">Languages: </span> {languages}
-          </p>
-        </div>
+              <div className="profile-stats-div">
+                <div>
+                  <p>
+                    <span className="data-label">Native Name: </span> {country.nativeName}
+                  </p>
+                  <p>
+                    <span className="data-label">Population: </span> {country.population}
+                  </p>
+                  <p>
+                    <span className="data-label">Region: </span> {country.continent}
+                  </p>
+                  <p>
+                    <span className="data-label">Sub Region: </span> {country.region}
+                  </p>
+                  <p>
+                    <span className="data-label">Capital: </span> {country.capital}
+                  </p>
+                </div>
 
-        <div>
-          <p>Border Countries: </p>
-          <ul>{neighborLinks}</ul>
+                <div>
+                  <p>
+                    <span className="data-label">Top Level Domain: </span> {country.topLevelDomain[0]}
+                  </p>
+                  <p>
+                    <span className="data-label">Currencies: </span> {currencies}
+                  </p>
+                  <p>
+                    <span className="data-label">Languages: </span> {languages}
+                  </p>
+                </div>
+              </div>
+
+              <div className="profile-neighbor-div">
+                <p>Border Countries: </p>
+                <ul>{neighborLinks}</ul>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
